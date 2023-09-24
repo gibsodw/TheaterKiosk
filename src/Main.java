9bbab571921be90456dd997f557655679b861d66
dongi <dongibson633@gmail.com>
2023-09-24 18:15:46 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int age = 0;
+
+        System.out.print("Enter your current age: ");
+
+        age = in.nextInt();
+        in.nextLine();
+        if(age >= 21)
+        {
+            System.out.println("You get a wrist band!");
+        }
+
+    }
+}
